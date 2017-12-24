7a660e384284674a39b16246e3cc2434d9503951
Andy Bulka <abulka@gmail.com>
2017-12-25 00:17:41 +1100

    more sophisticated scope and next_reg implementation

--- scope.py
+++ scope.py
@@ -8,32 +8,53 @@ config_log(log)
 """
 Scope is a dictionary and can contain name value pairs which map variables to register values
 Whenever we go nested, we create another scope which gets added to a scope stack, perhaps.
+
+The stack can be empty.
 """
 
 @attrs
 class ScopeStack(object):
     stack = attrib(default=Factory(list))
     _allow_mappings = attrib(default=False)
-    next_reg = attrib(default=0)
+
+    @property
+    def next_reg(self):
+        if len(self.stack) == 0:
+            return 0
+        else:
+            return self.stack[-1].next_reg
+
+    @next_reg.setter
+    def next_reg(self, val):
+        if len(self.stack):
+            self.stack[-1].next_reg = val
 
     def push(self):
-        self.stack.append(Scope(start_reg=self.next_reg))
+        if len(self.stack) == 0:
+            reg = 0
+        else:
+            reg = self.stack[-1].next_reg
+        self.stack.append(Scope(start_reg=reg, next_reg=reg))
 
     def pop(self):
-        self.next_reg = self.stack[-1].start_reg
-        self.stack.pop()
+        if len(self.stack) >= 2:
+            reg = self.stack[-1].start_reg
+            self.stack.pop()
+            self.stack[-1].next_reg = reg
+        else:
+            self.stack.pop()
 
     def add_mapping(self, var, register=None):
-        if register == None:
-            register = f'{self.next_reg:02d}'
-            self.next_reg += 1
-
-        if self._allow_mappings:
+        if not self._allow_mappings:
+            log.debug(f'scope mapping "{var}" to register "{register}" NOT allowed!!')
+        else:
+            if register == None:
+                register = f'{self.next_reg:02d}'
+                self.next_reg += 1
             log.debug(f'scope mapping "{var}" to register "{register}" allowed')
+
             scope = self.stack[-1]
             scope.var_to_registers[var] = register
-        else:
-            log.debug(f'scope mapping "{var}" to register "{register}" NOT allowed!!')
 
     def get_register(self, var):
         scope = self.stack[-1]
@@ -51,3 +72,4 @@ class ScopeStack(object):
 class Scope(object):
     var_to_registers = attrib(default=Factory(dict))
     start_reg = attrib(default=0)
+    next_reg = attrib(default=0)
