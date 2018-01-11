3e9c5154d5e795662f618e9921a0a1dacc2afd55
Andy Bulka <abulka@gmail.com>
2018-01-11 19:16:21 +1100

    example sync build_mappings() built

--- examples_sync.py
+++ examples_sync.py
@@ -1,24 +1,68 @@
 import json
 import os
-from attr import attrs, attrib, evolve
+from attr import attrs, attrib, evolve, Factory
 from examples import example_01
 from example_model import Example
 import logging
 from logger import config_log
+import pprint
 
 log = logging.getLogger(__name__)
 config_log(log)
 
 
 @attrs
+class ExampleProperties():
+    has_filename = attrib(default=False)
+    has_redis = attrib(default=False)
+    filename = attrib(default='')
+    redis_id = attrib(default='')
+    fingerprint = attrib(default='')
+
+@attrs
 class ExamplesSync():
     examples_dir = attrib(default='')
     is_production = attrib(default=False)
+    mappings = attrib(default=Factory(list))
 
     @classmethod
     def create(cls, app_dir, is_production):  # factory, cos no __init__
         examples_dir = os.path.join(app_dir, 'examples_json')
         return ExamplesSync(examples_dir, is_production)
+
+    def __attrs_post_init__(self):
+        self.build_mappings()
+
+    def build_mappings(self):
+
+        # Scan the files
+        files = os.listdir(self.examples_dir)
+        for filename in files:
+            ex = ExampleProperties(filename=filename)
+            with open(os.path.join(self.examples_dir, filename), 'r') as f:
+                s = f.read()
+                dic = json.loads(s)
+                ex.has_filename = True
+                ex.fingerprint = dic['fingerprint']
+            self.mappings.append(ex)
+
+        # Scan redis
+        for id in Example.ids():
+            example = Example.get(id)
+            found = False
+            for ex in self.mappings:
+                if example.fingerprint and ex.fingerprint == example.fingerprint:
+                    found = True
+                    break
+            if not found:
+                # ex was not created by a file - create an ex
+                ex = ExampleProperties()
+                ex.has_filename = False
+                self.mappings.append(ex)
+            ex.has_redis = True
+            ex.redis_id = id
+
+        pprint.pprint(self.mappings)
 
     def save_to_file(self, example):
         if self.is_production:
