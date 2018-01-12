693d5302afc81cc23949ff05ea4a62e88baa0bce
Andy Bulka <abulka@gmail.com>
2018-01-12 22:59:31 +1100

    logging to console on

--- settings.py
+++ settings.py
@@ -1,3 +1,9 @@
+import os
+
 LOG_FILENAME = 'debug.log'
-LOG_TO_CONSOLE = False
+LOG_TO_CONSOLE = True
 LOG_AST_CHILDREN_COMPLETE = False
+
+PRODUCTION = 'I_AM_ON_HEROKU' in os.environ
+if PRODUCTION:
+    LOG_TO_CONSOLE = True
