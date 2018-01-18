18d85de71523bb91e1603f6ae1027be78c7f5eb9
Andy Bulka <abulka@gmail.com>
2018-01-19 08:18:58 +1100

    find_line_comment() is now the only technique

--- rpn.py
+++ rpn.py
@@ -108,27 +108,14 @@ class RecursiveRpnVisitor(ast.NodeVisitor):
         return var_name == 'i'  # hack!  TODO - record this info in scope entry
 
     def find_comment(self, node):
-        # Finds comment in the original python source code associated with this token
-
-        # Technique 1
-        comment_ = self.atok.find_token(node.first_token, tokenize.COMMENT)  # requires asttokens >= 1.1.8
-
-        # Technique 1A
+        # Finds comment in the original python source code associated with this token.
+        # See my discussion of various techniques at https://github.com/gristlabs/asttokens/issues/10
         def find_line_comment(start_token):
             t = start_token
             while t.type not in (tokenize.COMMENT, tokenize.NL, tokenize.NEWLINE, tokenize.ENDMARKER):
                 t = self.atok.next_token(t, include_extra=True)
             return t.string if t.type == tokenize.COMMENT else ''
-        comment2 = find_line_comment(node.first_token)
-
-        # Technique 2
-        line = node.first_token.line
-        comment_i = line.find('#')
-        comment = line[comment_i:].strip() if comment_i != -1 else ''
-
-        # assert comment_.string == comment
-        assert comment2 == comment
-        return comment
+        return find_line_comment(node.first_token)
 
     # For visit support
 
