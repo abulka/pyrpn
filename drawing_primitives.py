0be322f7cd738bd34a089f2c4d51bb8ff6081167
Andy Bulka <abulka@gmail.com>
2018-01-25 17:14:34 +1100

    drawing rectangles works

--- drawing_primitives.py
+++ drawing_primitives.py
@@ -6,6 +6,7 @@ def dd1():
   draw_line(10, 10, 20, 5)
   draw_line(20, 5, 100, 16)
   draw_line(100, 16, 131, 1)
+  draw_rect(70, 4, 5, 5)
 
 def draw_line(x0, y0, x1, y1):  # rpn: int
     dx = ABS(x1 - x0)
@@ -36,4 +37,15 @@ def draw_line(x0, y0, x1, y1):  # rpn: int
             if e2 < dx:
                 err = err + dx
                 y0 = y0 + sy
+                
+# (x0, y0) = left top corner coordinates
+# w = width and h = height
+def draw_rect(x0, y0, w, h):  # rpn: int
+  #PRA('draw_rect', x0, y0, w, h);
+  y1 = y0 + h
+  draw_line(x0, y0, x0 + w, y0)  # top
+  draw_line(x0, y0, x0, y1)  # left
+  draw_line(x0, y0 + h, x0 + w, y1)  # bottom
+  draw_line(x0 + w, y0, x0 + w, y1)  # right
+
 """
