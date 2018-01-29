30f74458bc720bdc2fd76966397a82ec182b2304
Andy Bulka <abulka@gmail.com>
2018-01-29 11:33:17 +1100

    attempt at gooey-fying pyrpn, abandoned cos need framework python for wxpython

--- pyrpn.py
+++ pyrpn.py
@@ -2,36 +2,40 @@ import argparse
 from parse import parse
 import logging
 from logger import config_log
+# from gooey import Gooey
 
 log = logging.getLogger(__name__)
 config_log(log)
 
-parser = argparse.ArgumentParser(description="Convert python to rpn for hp41s/free42/dm42")
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-v", "--verbose", action="store_true")
-group.add_argument("-q", "--quiet", action="store_true")
-parser.add_argument("filename", type=str, help="the filename")
-parser.add_argument("-n", "--nolinenum", action='store_true', help="do not generate line numbers")
-parser.add_argument("-c", "--comments", action='store_true', help="generate commants")
-args = parser.parse_args()
+# @Gooey
+def main():
+    parser = argparse.ArgumentParser(description="Convert python to rpn for hp41s/free42/dm42")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--verbose", action="store_true")
+    group.add_argument("-q", "--quiet", action="store_true")
+    parser.add_argument("filename", type=str, help="the filename")
+    parser.add_argument("-n", "--nolinenum", action='store_true', help="do not generate line numbers")
+    parser.add_argument("-c", "--comments", action='store_true', help="generate commants")
+    args = parser.parse_args()
 
-# pprint.pprint(args, indent=4)
+    # pprint.pprint(args, indent=4)
 
-def run():
-    with open(args.filename) as fp:
-        source = fp.read()
-    program = parse(source)
-    rpn = program.lines_to_str(comments=args.comments, linenos=not args.nolinenum)
+    def run():
+        with open(args.filename) as fp:
+            source = fp.read()
+        program = parse(source)
+        rpn = program.lines_to_str(comments=args.comments, linenos=not args.nolinenum)
 
-    if args.quiet:
-        print(rpn)
-    elif args.verbose:
-        print('source is', source)
-        print('rpn is', rpn)
-    else:
-        line_count = len(rpn.split('\n'))
-        print(f'Generated {line_count} lines.')
-        print(rpn)
+        if args.quiet:
+            print(rpn)
+        elif args.verbose:
+            print('source is', source)
+            print('rpn is', rpn)
+        else:
+            line_count = len(rpn.split('\n'))
+            print(f'Generated {line_count} lines.')
+            print(rpn)
 
-run()
+    run()
 
+main()
