6680fd5009546c284fcb948735b3ad414892708f
Andy Bulka <abulka@gmail.com>
2018-02-01 20:41:16 +1100

    can't believe test_list_for_in_var_prompt_strings() passed

--- test_rpn2.py
+++ test_rpn2.py
@@ -1081,6 +1081,56 @@ class RpnTests2(BaseTest):
             """)
         self.compare(de_comment(expected))
 
+    def test_list_for_in_var_prompt_strings(self):
+        self.parse(dedent("""
+            a = ['aa', 'bb']
+            for el in a:
+                PROMPT(el)
+            """))
+        expected = dedent("""
+            0
+            SF 01
+            XEQ "pMxPrep"
+            "aa"
+            ASTO ST X
+            XEQ "LIST+"
+            "bb"
+            ASTO ST X
+            XEQ "LIST+"
+            RCL "ZLIST"
+            STO "a"
+
+            // setup for loop
+            RCL "a"         // prepare matrix
+            SF 01
+            XEQ "pMxPrep"
+            0               // from
+            XEQ "pMxLen"    // to
+            1               // step
+            XEQ "pISG"
+            STO 00            
+                        
+            LBL 00  // for
+            ISG 00  // test
+            GTO 01  // for body
+            GTO 02  // resume
+            LBL 01  // for body
+            
+            CLA
+            RCL 00  // get the index 
+            IP
+            RCL "a" // its an el index so prepare associated list for access
+            SF 01
+            XEQ "pMxPrep"
+            XEQ "p1MxIJ"
+            RCLEL   // get el
+            ARCL ST X
+            PROMPT
+            GTO 00  // for
+            LBL 02  // resume
+            """)
+        self.compare(de_comment(expected))
+
     @unittest.skip('so hard - need temp var for list itself')
     def test_list_for_in_literal_list(self):
         self.parse(dedent("""
