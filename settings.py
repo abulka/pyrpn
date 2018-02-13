31f7216470de3e0f64fc0f1d70dde1b068e44f69
Andy Bulka <abulka@gmail.com>
2018-02-13 13:41:45 +1100

    cleaned up settings and backward lookup etc

--- settings.py
+++ settings.py
@@ -27,86 +27,17 @@ LIST_UNSUPPORTED = ('cmp', 'index', 'count', 'extend', 'insert', 'remove', 'reve
 DICT_UNSUPPORTED = ('clear', 'copy', 'fromkeys', 'get', 'items', 'setdefault', 'update', 'values')
 MATRIX_UNSUPPORTED = ('INDEX', 'STOIJ', 'RCLIJ', 'PUTM', 'GETM', 'INSR', 'DELR', 'DIM', 'GROW', 'WRAP', 'SIMQ', 'GROW', 'WRAP')
 
-
-# So that converter knows that these are not user functions - cos these don't exist in PYTHON_CMD_TO_RPN
-PYLIB_INSERTABLE_WHEN_ORIGINAL_REMAPPED = {
-    'pFS': {'description': 'is flag set?'},
-    'pFC': {'description': 'is flag clear?'},
-    'pREAL': {'description': 'is Real?'},
-    'pBIT': {'description': 'test the xth bit of y.'},
-    'pCPX': {'description': 'is complex number?'},
-    'pMAT': {'description': 'is matrix?'},
-    'pSTR': {'description': 'is string?'},
-
-    'pMxLen': {'description': 'length of list or dict'},
-}
-
-# Python replacements for native HP42S commands which cannot be entered due to strange symbols
-# The converter maps the new Python name to the original name, or to a rpnlib function and inserts it.
-PYTHON_CMD_TO_RPN = {
-    'isFS':         'pFS',      # mapped to replacement rpnlib functions
-    'isFC':         'pFC',
-    'isREAL':       'pREAL',
-    'testBIT':      'pBIT',
-    'isCPX':        'pCPX',
-    'isMAT':        'pMAT',
-    'isSTR':        'pSTR',
-
-    'len':          'pMxLen',
-
-    'print':        'AVIEW',    # mapped to original HP42S commands
-    'Eto':          'E↑X',
-    'EtoMinus1':    'E↑X-1',
-    'LN1plus':      'LN1+X',
-    'Reciprocal':   '1/X',
-    'CommonExp':    '10↑X',
-    'toDEC':        '→DEC',
-    'toDEG':        '→DEG',
-    'toHMS':        '→HMS',
-    'toHR':         '→HR',
-    'toOCT':        '→OCT',
-    'toPOL':        '→POL',
-    'toRAD':        '→RAD',
-    'toREC':        '→REC',
-    'Percent':      '%',
-    'PercentCH':    '%CH',
-    'ALLStat':      'ALLΣ',
-    'LINStat':      'LINΣ',
-    'StatPlus':     'Σ+',
-    'StatMinus':    'Σ-',
-    'StatREG':      'ΣREG',
-    'StatWhichREG': 'ΣREG?',
-    'HMSplus':      'HMS+',
-    'HMSminus':     'HMS–',
-    'Factorial':    'N!',
-    'BASEplus':     'BASE+',
-    'BASEminus':    'BASE–',
-    'BASEtimes':    'BASEx',
-    'BASEdivide':   'BASE÷',
-    'BASEplusMinus':'BASE+/–',
-    'RDXcomma':     'RDX,',
-    'RDXperiod':    'RDX.',
-
-    'EDIT':         'EDITN',
-
-}
-# backward lookup so that cmd list gen can show the Python substitute in cmd list table
-RPN_CMD_TO_PYTHON_REPLACEMENT = {v: k for k, v in PYTHON_CMD_TO_RPN.items()}
-# import pprint
-# pprint.pprint(RPN_CMD_TO_PYTHON_REPLACEMENT)
-RPN_TO_PYTHON_CMD_ZZZ = {
-    '%':        'Percent',
+RPN_CMD_TO_PYTHON_REPLACEMENT = {
+    '%':        'Percent',          # mapped to original HP42S commands
     '%CH':      'PercentCH',
     '1/X':      'Reciprocal',
     '10↑X':     'CommonExp',
     'ALLΣ':     'ALLStat',
-    #'AVIEW': 'print',
     'BASE+':    'BASEplus',
     'BASE+/–':  'BASEplusMinus',
     'BASEx':    'BASEtimes',
     'BASE÷':    'BASEdivide',
     'BASE–':    'BASEminus',
-    #'EDITN': 'EDIT',
     'E↑X':      'Eto',
     'E↑X-1':    'EtoMinus1',
     'HMS+':     'HMSplus',
@@ -116,14 +47,6 @@ RPN_TO_PYTHON_CMD_ZZZ = {
     'N!':       'Factorial',
     'RDX,':     'RDXcomma',
     'RDX.':     'RDXperiod',
-    'pBIT':     'testBIT',
-    'pCPX':     'isCPX',
-    'pFC':      'isFC',
-    'pFS':      'isFS',
-    'pMAT':     'isMAT',
-    #'pMxLen':   'len',
-    'pREAL':    'isREAL',
-    'pSTR':     'isSTR',
     'Σ+':       'StatPlus',
     'Σ-':       'StatMinus',
     'ΣREG':     'StatREG',
@@ -135,10 +58,21 @@ RPN_TO_PYTHON_CMD_ZZZ = {
     '→OCT':     'toOCT',
     '→POL':     'toPOL',
     '→RAD':     'toRAD',
-    '→REC':     'toREC'
+    '→REC':     'toREC',
+
+    'AVIEW':    'print',            # extras
+    'EDITN':    'EDIT',
+    'pMxLen':   'len',
+
+    'pFS':      'isFS',             # mapped to replacement rpnlib functions
+    'pFC':      'isFC',
+    'pREAL':    'isREAL',
+    'pBIT':     'testBIT',
+    'pCPX':     'isCPX',
+    'pMAT':     'isMAT',
+    'pSTR':     'isSTR',
 }
-# Original HP42S commands to Python replacement (only used by cmd list gen)
-RPN_TO_RPNLIB_SPECIAL = {
+RPN_TO_RPNLIB_SPECIAL = {           # HP42S commands to Python replacement (only used by cmd list gen)
     'FS?':      'isFS',
     'FC?':      'isFC',
     'REAL?':    'isREAL',
@@ -147,7 +81,18 @@ RPN_TO_RPNLIB_SPECIAL = {
     'MAT?':     'isMAT',
     'STR?':     'isSTR',
 }
+PYTHON_CMD_TO_RPN = {v: k for k, v in RPN_CMD_TO_PYTHON_REPLACEMENT.items()}  # Handy backwards lookup
+PYLIB_INSERTABLE_WHEN_ORIGINAL_REMAPPED = {  # So that converter knows that these are not user functions - cos these don't exist in PYTHON_CMD_TO_RPN
+    'pFS': {'description': 'is flag set?'},
+    'pFC': {'description': 'is flag clear?'},
+    'pREAL': {'description': 'is Real?'},
+    'pBIT': {'description': 'test the xth bit of y.'},
+    'pCPX': {'description': 'is complex number?'},
+    'pMAT': {'description': 'is matrix?'},
+    'pSTR': {'description': 'is string?'},
 
+    'pMxLen': {'description': 'length of list or dict'},
+}
 NUM_PARAMS_UNSPECIFIED = -1
 
 # Specifications of RPN limitations
