30c8129ec9ba2ff4276aa73ef3f21fe701abd5cd
Andy Bulka <abulka@gmail.com>
2018-02-28 10:13:47 +1100

    tweaked log formats to make them colour

--- logger.py
+++ logger.py
@@ -11,9 +11,10 @@ def config_log(log):
     log.setLevel(logging.DEBUG)
 
     # formatter = logging.Formatter('%(asctime)-15s %(levelname)s %(message)s')
-    # formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(name)-10s %(levelname)5s %(message)s')
     # formatter = logging.Formatter('%(name)10s %(message)s')
-    formatter = logging.Formatter('%(message)s')
+    # formatter = logging.Formatter('%(message)s')   # <----- TRADITIONAL ONE I USED but doesn't work well with ideolog colour highlighter
+    # formatter = logging.Formatter('%(levelname)5s %(message)s')
 
     # create the logging file handler
     fh = logging.FileHandler(settings.LOG_FILENAME)
